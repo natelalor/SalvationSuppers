9459184aa1f561f8b6ee7cad86f8cd1de4a17c72
Ty Allembert <tyallembert@gmail.com>
2022-03-21 09:37:13 -0400

    python backend implementation using flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, jsonify,request
-from os import listdir
+from flask import Flask, render_template, redirect, jsonify,request
+import os
 import json
 import time
+
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -10,18 +13,49 @@ from email.mime.multipart import MIMEMultipart
 import smtplib
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "Ty": generate_password_hash("Ilikepie11")
+}
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 #startup
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST","GET"])
 def homeStartup():
     return render_template("index.html", title="index", name="Ty Allembert")
 #Index
-@app.route("/index.html", methods=["GET"])
+@app.route("/index.html", methods=["POST","GET"])
 def homeIndex():
     return render_template("index.html", title="index", name="Ty Allembert")
+
+#===admin page===
+@app.route("/admin.html", methods=["POST","GET"])
+@auth.login_required
+def admin():
+    return render_template("admin.html", title="admin", name="Ty Allembert")
+
+@app.route("/display_volunteers", methods=["POST","GET"])
+def display_volunteers():
+    ALL_VOLUNTEERS = []
+    volunteer = []
+    with open("form_data.txt", "r") as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            if line == "*":
+                ALL_VOLUNTEERS.append(volunteer)
+                volunteer = []
+            if line != "*":
+                volunteer.append(line.rstrip())
+
+    return jsonify(ALL_VOLUNTEERS)
+#===save form function===
 @app.route("/save_form", methods=["POST","GET"])
 def save_form():
-    print("got here")
     #the_array=request.get_json(force=True)
     the_array=request.form.getlist("form[]")
     print(the_array)
@@ -29,16 +63,16 @@ def save_form():
         for x in the_array:
             fo.write(str(x))
             fo.write(str("\n"))
-        fo.write(str("\n"))
+        fo.write(str("*\n"))
 
+#Start of sending email
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
     smtp.ehlo()
     smtp.starttls()
     smtp.login('SalvationSupperAuto@gmail.com', 'TAWikrEQs9Jjz9v')
 
     subject = "New Salvation Suppers Volunteer"
-    #text = "<p>A new volunteer, " + the_array[0] + " " + the_array[1] + " just signed up!</p>"
-    text = """\
+    html = """\
         <html>
         <head></head>
         <body>
@@ -50,7 +84,8 @@ def save_form():
         """
     msg = MIMEMultipart()
     msg['Subject'] = subject
-    msg.attach(MIMEText(text, 'html'))
+    msg['From'] = "Salvation Suppers"
+    msg.attach(MIMEText(html, 'html'))
 
     smtp.sendmail(from_addr="SalvationSupperAuto@gmail.com",
               to_addrs="tallembe@uvm.edu", msg=msg.as_string())
--- static/js/script.js
+++ static/js/script.js
@@ -1,45 +1,104 @@
 
 //window.addEventListener('load', image_transition);
-$("#submitButton").on("click", form_click);
+//$("#submitButton").on("click", form_click);
 $("#zero_gravity").on("mouseover", show_sponsor_name)
 $("#brio_coffee").on("mouseover", show_sponsor_name)
 $("#single_pebble").on("mouseover", show_sponsor_name)
 $("#slow_food").on("mouseover", show_sponsor_name)
-$("#submitButton").on("click",save_form);
+$("#submitButton").on("click", save_form)
+$("#show_volunteer_button").on("click", display_volunteers)
+
+var regEx = /^[0-9a-zA-Z]+$/;
+var email_validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var phone_validation = /^\d{10}$/;
 
 let first_name;
 let last_name;
 let email;
 let phone;
 let interests;
+let able_to_save = true;
 
  $("#txtFirstName").on("focusout", function(){
     first_name = $(this).val();
+    if(first_name.match(regEx)){
+        able_to_save = true;
+    }else{
+        $("#sign_up").append("<p class='validation_message'>First name can only have letters</p>")
+        able_to_save = false;
+    }
 });
 $("#txtLastName").on("focusout", function(){
     last_name = $(this).val();
+    if(last_name.match(regEx)){
+        able_to_save = true;
+    }else{
+        $("#sign_up").append("<p class='validation_message'>Last name can only have letters</p>")
+        able_to_save = false;
+    }
 });
 $("#txtEmail").on("focusout", function(){
     email = $(this).val();
+    if(email.match(email_validation)){
+        able_to_save = true;
+    }else{
+        $("#sign_up").append("<p class='validation_message'>Email is missing some required characters</p>")
+        able_to_save = false;
+    }
 });
 $("#telPhoneNumber").on("focusout", function(){
     phone = $(this).val();
+    if(phone.match(phone_validation)){
+        able_to_save = true;
+    }else{
+        $("#sign_up").append("<p class='validation_message'>Phone numbers can only have numbers, (), and -</p>")
+        able_to_save = false;
+    }
 });
 $("#txtInterest").on("focusout", function(){
     interests = $(this).val();
+    if(interests.match(regEx)){
+        able_to_save = true;
+    }else{
+        $("#sign_up").append("<p class='validation_message'>Your \"intereests\" has invalid characters</p>")
+        able_to_save = false;
+    }
 });
 function save_form(){
-    var form_array = [first_name, last_name, email, phone, interests];
-    console.log(form_array);
+    if(able_to_save){
+        $("#sign_up").empty();
+        $("#sign_up").addClass("submitted");
+        $("#sign_up").append('<h2>Thank You!</h2><h3>We will contact you shortly.</h3><!--<img src="images/happiness.png" alt="smile face">-->');
+
+        var form_array = [first_name, last_name, email, phone, interests];
+        console.log(form_array);
+        $.ajax({
+            url: "/save_form",
+            type: "post",
+            data:{form:form_array},
+            success: function(response) {
+                console.log(response);
+            }
+        });
+    }
+};
+function display_volunteers(){
+    $(".show_volunteer_container").remove();
     $.ajax({
-        url: "/save_form",
+        url: "/display_volunteers",
         type: "post",
-        data:{form:form_array},
         success: function(response) {
             console.log(response);
+            for(var i = 0; i < response.length; ++i){
+                $(".table_wrapper").append("<div class = 'all_volunteers' id='volunteer_"+i+"'></div>")
+                $("#volunteer_" + i).append("<p>"  +  response[i][0] + " " + response[i][1] + "</p>")
+                $("#volunteer_" + i).append("<p>"  +  response[i][2] +"</p>")
+                $("#volunteer_" + i).append("<p>"  +  response[i][3] +"</p>")
+                $("#volunteer_" + i).append("<p>"  +  response[i][4] +"</p>")
+            }
         }
     });
-};
+}
 //Image transition section
 var i = 0;
 setInterval(image_transition, 3200);
@@ -64,11 +123,11 @@ function image_transition() {
 }
 
 //form submitted animation
-function form_click() {
+/*function form_click() {
     $("#sign_up").empty();
     $("#sign_up").addClass("submitted");
     $("#sign_up").append('<h2>Thank You!</h2><h3>We will contact you shortly.</h3><!--<img src="images/happiness.png" alt="smile face">-->');
-}
+}*/
 //show sponsor
 function show_sponsor_name(){
     $(".zero_gravity_container>p").addClass("show_p")
